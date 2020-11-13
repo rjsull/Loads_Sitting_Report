68d1c46193195de85316738f4759a0fa586e8d0a
schnickens <34480861+schnickens@users.noreply.github.com>
2020-11-13 11:02:58 -0500

    Update G_P_Loads_Sitting_Report_Pull.bat

--- G_P_Loads_Sitting_Report_Pull.bat
+++ G_P_Loads_Sitting_Report_Pull.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\R\R-4.0.2\binRscript.exe" "C:\Users\sullivanry\Documents\Loads-Sitting-Report\G_P_Loads_Sitting_Report_Pull.R"
+"C:\Program Files\R\R-4.0.2\bin\Rscript.exe" "C:\Users\sullivanry\Documents\Loads-Sitting-Report\G_P_Loads_Sitting_Report_Pull.R"
 pause
